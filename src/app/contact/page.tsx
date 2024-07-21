a0a095610257f8a95473a8bee65e9f52b58d1cc5
Sejal Sinha <sejalsinha322@gmail.com>
2024-07-21 23:56:13 +0530

    Added some changes

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -83,9 +83,27 @@ export default function Contact() {
                     </Button>
                 </form>
               </div>
-            
+
+              {/* info */}
               <div className="flex-1 flex items-center xl:justify-end order-1 
               xl:order-none mb-8 xl:mb-0">
+                <ul className="flex flex-col gap-10">
+                  {info.map((item, index) => {
+                    return (
+                      <li key={index} className="flex items-center gap-6">
+                        <div className="w-[52px] h-[52px] xl:w-[72px]
+                        xl:h-[72px] bg-slate-200 text-black rounded-md flex items-center
+                        justify-center">
+                          <div>{item.icon}</div>
+                        </div>
+                        <div className="flex-1">
+                          <p className="text-slate-600">{item.title}</p>
+                          <h3 className="text-xl">{item.description}</h3>
+                        </div>
+                      </li>
+                    );
+                  })}
+                </ul>
               </div>
             </div>
           </div>
